6ccdbbcbe3c0a77bf15690391a1a68b1b76b63e8
John Azzaro <jazzaro@temple.edu>
2019-09-16 19:54:47 -0700

    adding post route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");                                   // load the express library
 const app = express();                                                // let app is equal to running the express function.
 
-const users = []                                                      // Users as a local variable (which would normally be in a database but this is for the study)
+const users = [{ name: "John Smith"}];                                 // Users as a local variable (which would normally be in a database but this is for the study)
 
 app.get('/users', function(req, res) {                                // create a route to get users
     res.json(users);                                                  // send response of users as a json object
