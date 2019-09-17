a0b70c92f822ea60aa7fba73000a2e7612807d78
John Azzaro <jazzaro@temple.edu>
2019-09-16 19:34:42 -0700

    get route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");                                   // load the express library
 const app = express();                                                // let app is equal to running the express function.
 
+app.get('users', function() {
+
+});
+
 app.listen(3000, function() {                                         //  listen on port 3000...
     console.log('You app is listening on port 3000...');
 })
