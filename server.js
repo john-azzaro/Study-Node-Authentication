de09817cf3cc67ff400f865fdba99a87da125524
John Azzaro <jazzaro@temple.edu>
2019-09-16 19:38:23 -0700

    modfications to get request

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");                                   // load the express library
 const app = express();                                                // let app is equal to running the express function.
 
-app.get('users', function() {
+const users = []                                                      // Users as a local variable (which would normally be in a database but this is for the study)
 
+app.get('/users', function(req, res) {                                // create a route to get users
+    res.json(users);                                                  // send response of users as a json object
 });
 
 app.listen(3000, function() {                                         //  listen on port 3000...
